375b7e811bd5d27938fb9590ee5667ce0295e3e9
maskeria <ashishmaskeri@gmail.com>
2019-02-19 00:18:10 -0800

    Colour functionality added, still need to make seperate slideColour function, so that it can be implemented in drawLine too

--- mypaint.py
+++ mypaint.py
@@ -3,14 +3,14 @@ import time
 pygame.init()
 
 display_width = 800
-display_height = 800
+display_height = 600
 
 #  COLOURS 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (200, 0, 0)
 green = (0, 200, 0)
-blue = (0, 0, 100)
+blue = (0, 0, 200)
 bright_red = (255, 0, 0)
 bright_green = (0, 255, 0)
 bright_blue = (0, 0, 255)
@@ -36,7 +36,9 @@ def button(msg, x, y, w, h, ic, ac, action = None):
 	click = pygame.mouse.get_pressed()
 	if x < mouse[0] < x+w and y < mouse[1] < y+h:
 		pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+		pygame.draw.rect(gameDisplay, ic, (x+5, y+5, w-10, h-10))
 		if click[0] == 1 and action != None:
+			pygame.display.update()
 			action()
 	
 	else:
@@ -63,13 +65,13 @@ def drawLine():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				quit()
-			
-			button("DRAW", 10, 10, 100, 30, green, bright_green, freeDraw)
+				
 			button("QUIT", 120, 10, 100, 30, red, bright_red, quit_paint)
-			button("Erase All", 10, 700, 100, 30, blue, bright_blue, eraseAll)
-		
+			button("Erase All", 10, 550, 100, 30, red, bright_red, eraseAll)
+			button("DRAW", 10, 10, 100, 30, green, bright_green, freeDraw)
+			
 			mouse = pygame.mouse.get_pos()
-		#click = pygame.mouse.get_pressed()
+			#click = pygame.mouse.get_pressed()
 		
 			if event.type == pygame.MOUSEBUTTONDOWN and one == False:
 				
@@ -77,12 +79,11 @@ def drawLine():
 				one = True
 				order = True
 					
-			if event.type == pygame.MOUSEBUTTONUP and order == True:
+			elif event.type == pygame.MOUSEBUTTONUP and order == True:
 				clickTwo = (mouse[0],  mouse[1])
 				two = True
 			
-			print(clickOne, clickTwo)
-			if one and two:	
+			elif one and two:	
 				pygame.draw.line(gameDisplay, white, clickOne, clickTwo, 5)
 				pygame.display.update()
 				one = False
@@ -90,34 +91,67 @@ def drawLine():
 				order = False
 				#line = False
 			
+				
 			
-		
+				
+			pygame.display.update()
 			clock.tick(500)	
 
 def freeDraw():
 	#add more functionality to colour
 	loop = True
+	r = 250
+	g = 250
+	b = 250
 	while loop:	
-		pygame.display.update()
+		
 		for event in pygame.event.get():
 			
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				quit()
 				
-		mouse = pygame.mouse.get_pos()
-		click = pygame.mouse.get_pressed()
-		
-		if click[0] == 1:
-			pygame.draw.rect(gameDisplay, red, (mouse[0], mouse[1], 3, 3))
-		elif click[2] == 1:
-			pygame.draw.rect(gameDisplay, black, (mouse[0]-25, mouse[1]-25, 50, 50))
-		
-		button("QUIT", 120, 10, 100, 30, red, bright_red, quit_paint)
-		button("Erase All", 10, 700, 100, 30, blue, bright_blue, eraseAll)
-		button("line", 240, 10, 100, 30, blue, bright_blue, drawLine)
-		pygame.display.update()
-		clock.tick(500)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					if b < 250 and r == 250 and g == 250:
+						r = 0
+						g = 0
+						b += 50
+					elif g < 250 and r == 250:
+						r = 0
+						g += 50
+					elif r < 250:
+						r += 50
+
+				print(event)
+				
+				if event.key == pygame.K_RIGHT:
+					if b > 0 and g >= 0 and r >= 0:
+						b -= 50
+					elif g > 0 and b >= 0:
+						b = 250
+						g -= 50
+					elif r > 0:
+						b = 250
+						g = 250
+						r -= 50
+			
+					
+			color = (r, g, b)	
+			print(color)
+			mouse = pygame.mouse.get_pos()
+			click = pygame.mouse.get_pressed()
+
+			if click[0] == 1:
+				pygame.draw.rect(gameDisplay, color, (mouse[0], mouse[1], 3, 3))
+			elif click[2] == 1:
+				pygame.draw.rect(gameDisplay, black, (mouse[0]-25, mouse[1]-25, 50, 50))
+			
+			button("QUIT", 120, 10, 100, 30, red, bright_red, quit_paint)
+			button("Erase All", 10, 550, 100, 30, red, bright_red, eraseAll)
+			button("line", 240, 10, 100, 30, blue, bright_blue, drawLine)
+			pygame.display.update()
+			clock.tick(500)
 		
 		
 def eraseAll():
