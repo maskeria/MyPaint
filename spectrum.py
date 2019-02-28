f12c92c29c1189a4a8fd1f89b6577b224311b96f
maskeria <ashishmaskeri@gmail.com>
2019-02-27 22:34:14 -0800

    spectrum function works, returns chosen color, still need to eliminate repititions and then incorporate into mypaint

--- spectrum.py
+++ spectrum.py
@@ -13,7 +13,7 @@ def spectrum():
 	green = []
 	blue = []
 	
-	resolution = 40
+	resolution = 50
 	for r in range(0, 255, resolution):
 		red.append(r)
 	for g in range(0, 255, resolution):
@@ -21,7 +21,9 @@ def spectrum():
 	for b in range(0, 255, resolution):
 		blue.append(b)
 		
-	
+	colors = []
+	colorPosx = []
+	colorPosy = []
 	
 	w = 20
 	xpos = 0
@@ -30,25 +32,52 @@ def spectrum():
 	for x in range(0, len(red)):
 		for y in range(0, len(green)):
 			for z in range(0, len(blue)):
-				print((red[x],green[y],blue[z]))
-				pygame.draw.rect(gameDisplay, (red[x],green[y],blue[z]), (xpos, ypos, w, w))
-				pygame.display.update()
-				if xpos < ((250/resolution)+1)**3:
-					xpos += w
-				else:
-					xpos = 0
-					ypos += w
-
-loop = True					
-while True:
+				 colors.append((red[x],green[y],blue[z]))
+				 
+	for i in range(0, len(colors)):
+		pygame.draw.rect(gameDisplay, colors[i], (xpos, ypos, w, w))
+		pygame.display.update()
+		colorPosx.append(xpos)
+		colorPosy.append(ypos)
+		
+		if xpos < ((250/resolution)+1)**3:
+			xpos += w
+		else:
+			xpos = 0
+			ypos += w
+				
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			quit()
-		if loop:					
-			spectrum()		
-			loop = False
-		else:
-			print("Hi")
+		
+		choice = True
+		
+		
+		
+		i = 0
+		while choice:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					quit()
+					
+			mouse = pygame.mouse.get_pos()
+			click = pygame.mouse.get_pressed()
+			#print(colors[i], colorPosx[i], colorPosy[i], i)
+			
+			if colorPosx[i] < mouse[0] < colorPosx[i] + w and colorPosy[i] < mouse[1] < colorPosy[i] + w: 
+				
+				if click[0] == 1:
+					return colors[i]
+					
+					choice = False
+			
+			if i < len(colors)-1:
+				i += 1
+			else:
+				i = 0
+					
+print(spectrum())
 
 		
